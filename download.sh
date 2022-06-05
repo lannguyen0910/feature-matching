360da75dbf3c9f2acec7f395ed153627ce44d0d3
lannguyen <18120051@student.hcmus.edu.vn>
2022-06-05 09:51:58 +0700

    update download quadtree pretrained

--- download.sh
+++ download.sh
@@ -4,10 +4,13 @@ cd ${FOLDER_PATH}
 kaggle competitions download -c image-matching-challenge-2022
 kaggle datasets download -d ammarali32/kornia-loftr
 kaggle datasets download -d losveria/super-glue-pretrained-network
+kaggle datasets download -d optimo/quadtreecheckpoints
+unzip quadtreecheckpoints.zip -d QuadTreeAttention
 unzip image-matching-challenge-2022.zip -d image-matching-challenge-2022
 unzip kornia-loftr.zip -d kornia-loftr
-unzip super-glue-pretrained-network.zip -d models/SuperGluePretrainedNetwork
-cp super-glue-pretrained-network/models/superglue.py LoFTR/src/loftr/utils/
+unzip super-glue-pretrained-network.zip -d SuperGluePretrainedNetwork
+cp SuperGluePretrainedNetwork/models/superglue.py LoFTR/src/loftr/utils/
+cp QuadTreeAttention/outdoor_quadtree.ckpt models/QuadTree/pretrained/
 rm image-matching-challenge-2022.zip
 rm super-glue-pretrained-network.zip
 rm kornia-loftr.zip
